746df27adff5f10ea8898ab9cd944742ab039fe8
boqiling <boqiling@gmail.com>
2014-01-21 15:12:35 +0800

    add timeout and problem13

--- problem10.py
+++ problem10.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from timeout import timethis
 
 
+def isprime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in xrange(3, int(n ** 0.5) + 1, 2):  # skip 1 number at a time
+        if n % i == 0:
+            return False
+    return True
+
+
+def nextprime(n):
+    while True:
+        n += 1
+        if isprime(n):
+            return n
+
+
+@timethis
 def problem10():
     """
     The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
     Find the sum of all the primes below two million.
     """
     limit = 2000000
-
-    def isprime(n):
-        if n <= 1:
-            return False
-        if n == 2:
-            return True
-        if n % 2 == 0:
-            return False
-        for i in xrange(3, int(n ** 0.5) + 1, 2):  # skip 1 number at a time
-            if n % i == 0:
-                return False
-        return True
-
-    def nextprime(n):
-        while True:
-            n += 1
-            if isprime(n):
-                return n
 
     curr = 2
     result = 0
--- problem5.py
+++ problem5.py
@@ -9,15 +9,20 @@ def problem5():
     What is the smallest positive number that is evenly divisible by all of
     the numbers from 1 to 20?
     """
+    limit = 20
+
     def gcd(a, b):
+        #最大公约数
         while(b > 0):
             a, b = b, a % b
         return a
 
     def lcm(a, b):
+        #最小公倍数
         return (a * b) / gcd(a, b)
 
-    return reduce(lcm, range(1, 21), 1)
+    # 依次求1和2，直到20的最小公倍数
+    return reduce(lcm, range(1, limit + 1), 1)
 
 
 if __name__ == "__main__":
